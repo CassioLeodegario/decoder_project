eda48aea2d0a4f20e13b7b3a3da7d34c6ba651d5
CassioLeodegario <cassio.leodegario@gmail.com>
2021-11-25 20:07:26 -0300

    feat: general commit

--- apifirst/src/main/java/com/apifirst/controllers/LibraryController.java
+++ apifirst/src/main/java/com/apifirst/controllers/LibraryController.java
@@ -1,2 +1,25 @@
-package com.apifirst.controllers;public class LibraryController {
+package com.apifirst.controllers;
+
+import com.apifirst.libraryservice.api.LibraryApi;
+import com.apifirst.libraryservice.models.Book;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class LibraryController implements LibraryApi {
+
+    @Override
+    public ResponseEntity<List<Book>> getAllBooksInLibrary() {
+        Book book = new Book();
+        book.setBookAuthor("Test");
+        book.setName("test");
+
+        List<Book> books = new ArrayList<>(Arrays.asList(book));
+
+        return ResponseEntity.ok(books);
+    }
 }
