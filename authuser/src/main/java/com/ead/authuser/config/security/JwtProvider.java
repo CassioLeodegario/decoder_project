12cf434a2e17212c1ac1ef3bb714c80868522eea
CassioLeodegario <cassio.leodegario@gmail.com>
2022-07-04 20:12:45 -0400

    feat: add jwt filter

--- authuser/src/main/java/com/ead/authuser/config/security/JwtProvider.java
+++ authuser/src/main/java/com/ead/authuser/config/security/JwtProvider.java
@@ -1,7 +1,6 @@
 package com.ead.authuser.config.security;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
@@ -29,5 +28,27 @@ public class JwtProvider {
                 .setExpiration(new Date(new Date().getTime() + jwtExpiration))
                 .signWith(SignatureAlgorithm.HS512, jwtSecret)
                 .compact();
+    }
+
+    public String getUsernameJwt(String token){
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateJwt(String authToken){
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (ExpiredJwtException e) {
+            log.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            log.error("JWT  is unsuported : {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+            log.error("Invalid JWT : {}", e.getMessage());
+        } catch (SignatureException e) {
+            log.error("Invalid JWT signature: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            log.error("JWT Claims String is empty: {}", e.getMessage());
+        }
+        return false;
     }
 }
