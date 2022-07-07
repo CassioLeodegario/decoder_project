65a76ab427a324154030f3eb34faac2cadb702d1
CassioLeodegario <cassio.leodegario@gmail.com>
2022-07-07 08:00:32 -0400

    feat: allow only admin to create new instructor

--- authuser/src/main/java/com/ead/authuser/controllers/InstructorController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/InstructorController.java
@@ -11,6 +11,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -30,6 +31,7 @@ public class InstructorController {
     @Autowired
     RoleService roleService;
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping("/subscription")
     public ResponseEntity<?> saveSubscriptionInstructor(
             @RequestBody @Valid InstructorDto instructorDto
